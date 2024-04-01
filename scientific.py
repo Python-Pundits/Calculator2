a0692b0e6d778769a08e68626c9c7138ed61b7a5
CeoAntidote <138785986+CeoAntidote@users.noreply.github.com>
2024-04-01 07:45:41 +0000

    scientific feature added || Digitals

---
+++ scientific.py
@@ -0,0 +1,45 @@
+import math
+
+# functions
+def sin(a):
+    return math.sin(a)
+
+def log(a):
+    return math.log(a)
+
+def tan(a):
+    return math.tan(a)
+
+def cos(a):
+    return math.cos(a)
+
+def sqrt(a):
+    return math.sqrt(a)
+
+exit = True
+
+# user input
+def operate():
+    operation = input("Select operation [sin, log, tan, cos, sqrt, exit]: ")
+    number = int(input("Input number: "))
+
+    if operation == "sin":
+        result = sin(number)
+    elif operation == "log":
+        result = log(number)
+    elif operation == "tan":
+        result = tan(number)
+    elif operation == "cos":
+        result = cos(number)
+    elif operation == "sqrt":
+        result = sqrt(number)
+    elif operation == "exit":
+        quit()
+
+    # output
+    print(f"Answer = {result}")
+
+# app re-render
+while exit == True:
+    operate()
+
